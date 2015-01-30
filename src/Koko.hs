263fd51bd77aa035f206a167c03042947d13d36e
Mikael Brockman <mikael@brockman.se>
2015-01-30 22:43:20 +0100

    Make a crazy parser
    It's a piece of cake to bake a pretty cake.

--- src/Koko.hs
+++ src/Koko.hs
@@ -1,10 +1,15 @@
 module Koko where
 
+import Control.Applicative
+
+import qualified Text.Parsec.Combinator as P
 import qualified Text.Parsec.Error as P
 import qualified Text.Parsec.Pos as P
 import qualified Text.Parsec.Prim as P
 
 data Expr = EVar String
+          | EApp Expr [Expr]
+          | ESym String
           deriving (Eq, Show)
 
 type Token = String
@@ -13,18 +18,17 @@ type Stream = [Token']
 type Parser a = P.Parsec Stream () a
 
 tokenThat :: (Token -> Bool) -> Parser Token
-tokenThat p = P.token (show . snd) fst p'
+tokenThat p = P.token show fst p'
   where p' (_, t) = if p t then Just t else Nothing
 
-class Result r where
-  fromEither :: Either P.ParseError e -> r e
-
-instance Result Maybe where
-  fromEither = either (const Nothing) Just
-
-parse :: Result r => [Token] -> r Expr
-parse xs = fromEither $ P.parse expr "" xs'
-  where xs' = ([(P.newPos "" 1 i, x) | x <- xs, i <- [1..]])
+parse :: [Token] -> Either P.ParseError Expr
+parse xs = P.parse expr "" xs'
+  where xs' = [(P.newPos "" 1 i, x) | (x, i) <- zip xs [1..]]
 
 expr :: P.Parsec [Token'] () Expr
-expr = tokenThat (const True) >> return (EVar "@foo")
+expr = P.choice [application, simple]
+  where
+    application = P.try $ EApp <$> variable <*> P.many simple
+    simple      = P.choice [variable, symbol]
+    variable    = EVar <$> tokenThat ((== "@") . take 1)
+    symbol      = ESym <$> tokenThat ((/= "@") . take 1)
--- test/Tests.hs
+++ test/Tests.hs
@@ -1,8 +1,18 @@
 import Test.Hspec
+
 import qualified Koko as K
 
 main :: IO ()
 main = hspec $ do
   describe "parser" $ do
     it "should parse `@foo'" $ do
-      K.parse ["@foo"] `shouldBe` (Just (K.EVar "@foo"))
+      ["@foo"] `shouldParseTo` (K.EApp (K.EVar "@foo") [])
+    it "should parse `@print Hello, world!'" $ do
+      ["@print", "Hello,", "world!"] `shouldParseTo`
+        (K.EApp (K.EVar "@print") [K.ESym "Hello,", K.ESym "world!"])
+
+shouldParseTo :: [K.Token] -> K.Expr -> Expectation
+shouldParseTo xs e =
+  case K.parse xs of
+   Left err -> expectationFailure (show err)
+   Right e' -> e' `shouldBe` e
