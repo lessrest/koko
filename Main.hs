7e668290bdaeb4da6132f24e4c6ce6f721f3724c
Mikael Brockman <mikael@brockman.se>
2015-02-03 19:32:42 +0100

    Add `tryIt` to Main module for debugging

--- Main.hs
+++ Main.hs
@@ -10,3 +10,7 @@ main = getArgs >>= (either (fail . show) ((run =<<) . evaluateIO)) . parse
   where run (Left v) = print v >> exitFailure
         run (Right _) = return ()
                         
+tryIt :: String -> IO ()
+tryIt = (either (fail . show) ((run =<<) . evaluateIO)) . parse . words
+  where run (Left v) = print v
+        run (Right _) = return ()
